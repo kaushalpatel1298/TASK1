51aa3e142ade6d136c72c31167d7a8aef844075b
kaushalpatel1298 <53890422+kaushalpatel1298@users.noreply.github.com>
2021-08-05 09:50:22 +0530

    Delete Assignment directory

--- Assignment/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const userController = require('../controllers/userController')
-
-const router = express.Router();
-
-router.route("/").get(userController.getUser);
-router.route("/signup").post(userController.signUp);
-
-module.exports = router;
